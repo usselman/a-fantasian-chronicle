bcb6500fd7be8bce7c9ef6984f2ac10a2b594d7e
usselman <sarkar.shirish@gmail.com>
2023-07-07 22:38:45 -0700

    tracery init

---
+++ main.js
@@ -0,0 +1,31 @@
+// Create a Tracery grammar
+const grammar = tracery.createGrammar({
+    hymn: ['#greeting#, #greeting#!', 'Sing #emotion#ly, #entity#!', 'Praise the #entity#!'],
+    greeting: ['Hallelujah', 'Hosanna', 'Blessed be', 'Rejoice'],
+    emotion: ['joyful', 'exuberant', 'heavenly'],
+    entity: ['sun', 'divine', 'glory'],
+    history: ['#event# happened in #year#, #event# in #year#, #event# in #year#.', 'Long ago, #event# occurred.'],
+    event: ['a great battle', 'the rise of a kingdom', 'a major discovery', 'a catastrophic event'],
+    year: ['100 BC', 'in ancient times', 'during the Renaissance', 'in the 21st century'],
+    myth: ['#story.capitalize#'],
+    story: ['#subject# #verb# #object#.', 'The #entity# #verb# #subject# and #object#.'],
+    subject: ['gods', 'heroes', 'titans', 'dragons'],
+    verb: ['created', 'fought', 'ruled', 'brought', 'slew'],
+    object: ['world', 'balance', 'harmony', 'prosperity'],
+});
+
+// Generate a new text based on the selected type and display it in the text container
+function generateText() {
+    const types = ['hymn', 'history', 'myth'];
+    const selectedType = types[Math.floor(Math.random() * types.length)];
+    const text = grammar.flatten(`#${selectedType}#`);
+    const textContainer = document.getElementById('textContainer');
+    textContainer.textContent = text;
+}
+
+// Attach event listener to the generate button
+const generateButton = document.getElementById('generateButton');
+generateButton.addEventListener('click', generateText);
+
+// Generate initial text on page load
+generateText();
