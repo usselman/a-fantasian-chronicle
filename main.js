b0d1bcaa996e7149b42f31d5544a1c6a78062711
usselman <sarkar.shirish@gmail.com>
2023-07-08 00:52:18 -0700

    wtf

--- main.js
+++ main.js
@@ -6,40 +6,25 @@ window.onload = function () {
     // Create a Tracery grammar
     const grammar = tracery.createGrammar({
         //hymn
-        hymn: ['Sing #emotion#ly, for #entity#!', 'In #entity#, we find our #blessing#!', 'Praise the #entity#, the #quality# one!', '#greeting#, #greeting#, to the #entity#!'],
-        greeting: ['Hosanna', 'Blessed be', 'Rejoice', 'In wisdom\'s wake'],
-        emotion: ['joyful', 'exuberant', 'heavenly', 'harmonious'],
-        entity: ['St. Elian', 'St. Alyra', 'St. Lucian', 'St. Meliora', 'St. Eosphoros', 'St. Talia', 'St. Raphael', 'St. Serene', 'St. Theodora', 'St. Uriel'],
-        blessing: ['light', 'harmony', 'wisdom', 'completion', 'peace', 'guidance', 'generosity', 'unity', 'enlightenment', 'balance'],
-        quality: ['enlightened', 'harmonious', 'watchful', 'fulfilled', 'peaceful', 'seeking', 'giving', 'united', 'wise', 'balanced'],
-
+        hymn: ['#greeting#, #greeting#!', 'Sing #emotion#ly, #entity#!', 'Praise the #entity#!'],
+        greeting: ['Hallelujah', 'Hosanna', 'Blessed be', 'Rejoice'],
+        emotion: ['joyful', 'exuberant', 'heavenly'],
+        entity: ['sun', 'divine', 'glory'],
         //history
-        history: ['In the age of #year#, #entity# #verb# #event#.', 'Remember when #entity# #verb# #event# in the time of #year#.', 'The annals of time recall #entity# and their #event# in the days of #year#.'],
-        event: ['a spiritual awakening', 'the founding of an Order', 'a great revelation', 'the cosmic alignment'],
-        year: ['the Great Eclipse', 'the Harmonic Concordance', 'the Dreamer\'s Epiphany', 'the Sephiratic Renaissance'],
-        verb: ['ushered in', 'led to', 'gave birth to', 'marked'],
-
+        history: ['#event# happened in #year#, #event# in #year#, #event# in #year#.', 'Long ago, #event# occurred.'],
+        event: ['a great battle', 'the rise of a kingdom', 'a major discovery', 'a catastrophic event'],
+        year: ['100 BC', 'in ancient times', 'during the Renaissance', 'in the 21st century'],
         //myth
-        myth: ['Once, #entity# journeyed to the #object#. #detail#. This is the tale of #concept#.', 'In ancient days, #entity# #verb# the #object#. #detail#. Thus began the era of #concept#.'],
-        object: ['Aeon Watch', 'Divine Completion', 'Sephiratic path', 'Cosmic Harmony', 'Dreamer\'s Wake', 'Illuminated Path', 'Assembly of Giving'],
-        verb: ['discovered', 'mastered', 'traversed', 'founded', 'embraced', 'revealed', 'established'],
-        detail: ['Their journey became our path', 'Their wisdom became our guide', 'Their revelation illuminated our world', 'Their harmony became our melody'],
-        concept: ['enlightenment', 'harmony', 'watchfulness', 'completion', 'peacefulness', 'seeking', 'giving', 'unity', 'wisdom', 'balance'],
-
-        //hadith
-        hadith: ['"Seek not only #concept#, but understand its source. To truly know the divine, one must comprehend both #concept# and #quality#."',
-            '"#concept.capitalize# is not static; it flows as a river, just as #quality# warms the heart and quenches the thirst of the soul."',
-            '"Time weaves its own tale, and #concept# whisper their secrets. Be a silent observer, and you shall hear the cosmic echoes."',
-            '"#concept.capitalize# is not an end, but a journey. Traverse the #object# and find the wholeness within."',
-            '"Seek not to wake the dreamer with force, but through harmonious vibrations. Be the #concept# in the cosmic discordance."'],
-        concept: ['light', 'balance', 'dreams', 'completion', 'harmony', 'seeking', 'generosity', 'unity', 'wisdom', 'balance'],
-        quality: ['generosity', 'peacefulness', 'observation', 'journey', 'calming melody'],
+        myth: ['#story.capitalize#'],
+        story: ['#subject# #verb# #object#.', 'The #entity# #verb# #subject# and #object#.'],
+        subject: ['gods', 'heroes', 'titans', 'dragons'],
+        verb: ['created', 'fought', 'ruled', 'brought', 'slew'],
+        object: ['world', 'balance', 'harmony', 'prosperity'],
     });
-
 
     // Generate a new text based on the selected type and display it in the text container
     function generateText() {
-        const types = ['hymn', 'history', 'myth', 'hadith'];
+        const types = ['hymn', 'history', 'myth'];
         const selectedType = types[Math.floor(Math.random() * types.length)];
         const text = grammar.flatten(`A ${selectedType}: #${selectedType}#`);
         const textContainer = document.getElementById('textContainer');
